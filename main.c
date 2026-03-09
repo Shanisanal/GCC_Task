c7c110e1df02ea33689a1fb21bb265843ceb3473
Shanisanal <shani.sanal@trenser.com>
2026-03-09 08:45:39 +0000

    Code cleanup

--- main.c
+++ main.c
@@ -19,32 +19,55 @@
 //******************************* Local Types ********************************* 
  
 //***************************** Local Constants ******************************* 
- 
+ #define NUM_ZONES           3
+
 //***************************** Local Variables ******************************* 
  
 //****************************** Local Functions ****************************** 
 
-//****************************** main ******************************
+//****************************** main *****************************************
 // Purpose : Continuously display formatted time information in multiple timezones
 //           (GMT, IST, PST) along with optional epoch time. The display refreshes
 //           every second to show real-time updates.
 // Inputs  : None
 // Outputs : None
-// Return  : int - Returns 0 upon successful completion.
+// Return  : Returns 0 upon successful completion.
 // Notes   : 
-//   - Calls DisplayCurrentTime() to print the current time details.
+//   - Calls GetCurrentTimeDate() to print the current time details.
 //   - Uses sleep(1) to refresh the output every second.
 //   - Runs indefinitely until manually terminated.
 //*****************************************************************************
 
 int main(void)
 {
+    TimeDisplayInfo stTimeInfo[NUM_ZONES] = {0};
+    bool blDisplaySuccess = false;
+   
     while(1)
     {
-        printf("==================================\n");
-        DisplayCurrentTime();   
-        printf("==================================\n\n");
-        sleep(1);   
+       blDisplaySuccess = GetCurrentTimeDate(stTimeInfo);
+
+       if(blDisplaySuccess == true)
+       {
+            printf("==================================\n");
+
+            for(int i = 0; i < NUM_ZONES; i++)
+            {
+                printf("%s\n", stTimeInfo[i].ucLabel);
+                printf("----------------\n");
+                printf("Time : %s\n", stTimeInfo[i].ucTime);
+                printf("Date : %s\n", stTimeInfo[i].ucDate);
+                
+                if(stTimeInfo[i].blShowEpoch)
+                {
+                    printf("Epoch: %ld\n", stTimeInfo[i].lEpoch);
+                }
+                printf("\n");
+            }
+
+            printf("==================================\n\n");
+       }
+       sleep(1);   
     }
     return 0;
 }
