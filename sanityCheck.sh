10ed1792676812dce843d1272dbf4d3a97f7035d
Shanisanal <shani.sanal@trenser.com>
2026-03-09 10:53:11 +0000

    Did static analysis

--- sanityCheck.sh
+++ sanityCheck.sh
@@ -6,8 +6,14 @@ git checkout develop
 # Pull the latest code
 git pull origin develop
 
+#do static analysis
+cppcheck -I /usr/include --std=c99 --quiet --enable=warning --error-exitcode=1 --suppress=toomanyconfigs .
+
 # Clean previous builds
 make clean
 
 # Build the project
 make
+
+
+
