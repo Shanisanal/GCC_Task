c49040689fdf81804a48da064a538e43cef65c21
Shanisanal <shani.sanal@trenser.com>
2026-03-10 05:23:16 +0000

    add sanityCheck.sh to automate build, static analysis, and leak detection

--- sanityCheck.sh
+++ sanityCheck.sh
@@ -1,19 +1,68 @@
 #!/bin/bash
 
+REPORT="report.txt"
+TARGET="./appTimer.exe"
+
+echo "===== Sanity Check Report =====" > $REPORT
+echo "Generated on: $(date +%F)" >> $REPORT
+echo "------------------------------" >> $REPORT
+
 # Checkout the develop branch
-git checkout develop
+echo ">>> Pulling latest code..." | tee -a $REPORT
+git checkout develop >> $REPORT 2>&1
 
 # Pull the latest code
-git pull origin develop
+git pull origin develop >> $REPORT 2>&1
+echo "" >> $REPORT
 
 #do static analysis
-cppcheck -I /usr/include --std=c99 --quiet --enable=warning --error-exitcode=1 --suppress=toomanyconfigs .
+echo ">>> Running static analysis (cppcheck)..." | tee -a $REPORT
+cppcheck -I /usr/include --std=c99 --quiet --enable=warning \
+    --error-exitcode=1 --suppress=toomanyconfigs . >> $REPORT 2>&1
+echo "" >> $REPORT
 
 # Clean previous builds
-make clean
+echo ">>> Building project..." | tee -a $REPORT
+make clean >> $REPORT 2>&1
 
 # Build the project
-make
+make >> $REPORT 2>&1
+BUILD_STATUS=$?
+
+if [ $BUILD_STATUS -ne 0 ]; then
+    echo "Build failed" | tee -a $REPORT
+    echo "Check report.txt for details" 
+    exit 1
+else
+    echo "Build succeeded." | tee -a $REPORT
+fi
+echo "" >> $REPORT
+
+#Do Dynamic analysis
+echo ">>> Running Dynamic Analysis (Valgrind)..." | tee -a $REPORT
+if [ -f "$TARGET" ]; then
+    timeout 5s valgrind --leak-check=full --show-leak-kinds=all \
+     --errors-for-leak-kinds=definite --error-exitcode=1 \
+    $TARGET >> $REPORT 2>&1
+    
+    VALGRIND_RESULT=$?
+    if [ $VALGRIND_RESULT -eq 0 ]; then
+        echo "No memory leaks detected." | tee -a $REPORT
+    elif [ $VALGRIND_RESULT -eq 124 ]; then
+        echo "Test timed out " | tee -a $REPORT
+        echo "Check report.txt for valgrind result summary" 
+    else
+        echo "Memory leaks or errors detected!" | tee -a $REPORT
+        echo "Check report.txt for leak summary " 
+    fi
+else
+    echo "Error: $TARGET not found. Build may have failed." | tee -a $REPORT
+fi
+echo "" >> $REPORT
+
+echo "===== End of Report =====" >> $REPORT
+
+
 
 
 
